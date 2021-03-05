8e9403b34585743c275adb276875ff04a56de4a6
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-03-05 14:54:03 +0900

    move _importnodes and _importnames

--- src/Utils.jl
+++ src/Utils.jl
@@ -7,41 +7,6 @@ struct DB
 end
 
 function DB(nodes_dmp::String, names_dmp::String)
-    function _importnodes(nodes_dmp_path::String)
-        parents = Dict{Int,Int}()
-        ranks = Dict{Int,Symbol}()
-
-        f = open(nodes_dmp_path, "r")
-        for line in readlines(f)
-            lines = split(line, "\t")
-            taxid = parse(Int, lines[1])
-            parent = parse(Int, lines[3])
-            rank = Symbol(lines[5])
-
-            parent != taxid || continue
-
-            parents[taxid] = parent
-            ranks[taxid] = rank
-        end
-        close(f)
-        return parents, ranks
-    end
-
-    function _importnames(names_dmp_path::String)
-        namaes = Dict{Int,String}()
-        f = open(names_dmp_path, "r")
-        for line in readlines(f)
-            lines = split(line, "\t")
-            if lines[7] == "scientific name"
-                taxid = parse(Int, lines[1])
-                name = lines[3]
-                namaes[taxid] = name
-            end
-        end
-        close(f)
-        return namaes
-    end
-
     @assert isfile(nodes_dmp)
     @assert isfile(names_dmp)
 
@@ -50,6 +15,43 @@ function DB(nodes_dmp::String, names_dmp::String)
 
     return DB(nodes_dmp, names_dmp, parents, ranks, namaes)
 end
+
+function _importnodes(nodes_dmp_path::String)
+    parents = Dict{Int,Int}()
+    ranks = Dict{Int,Symbol}()
+
+    f = open(nodes_dmp_path, "r")
+    for line in readlines(f)
+        lines = split(line, "\t")
+        taxid = parse(Int, lines[1])
+        parent = parse(Int, lines[3])
+        rank = Symbol(lines[5])
+
+        parent != taxid || continue
+
+        parents[taxid] = parent
+        ranks[taxid] = rank
+    end
+    close(f)
+    return parents, ranks
+end
+
+
+function _importnames(names_dmp_path::String)
+    namaes = Dict{Int,String}()
+    f = open(names_dmp_path, "r")
+    for line in readlines(f)
+        lines = split(line, "\t")
+        if lines[7] == "scientific name"
+            taxid = parse(Int, lines[1])
+            name = lines[3]
+            namaes[taxid] = name
+        end
+    end
+    close(f)
+    return namaes
+end
+
 
 function DB(db_path::String, nodes_dmp::String, names_dmp::String)
     @assert ispath(db_path)
