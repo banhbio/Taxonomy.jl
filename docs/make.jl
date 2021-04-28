209f0d7211a9ec337558a947ee450522ea45e192
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-04-28 11:27:41 +0900

    add function readme2index()

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,25 @@ using Documenter
 
 DocMeta.setdocmeta!(Taxonomy, :DocTestSetup, :(using Taxonomy); recursive=true)
 
+function readme2index()
+    readme_path = "README.md"
+    index_path = "docs/src/index.md"
+
+    try
+        f = open(readme_path)
+        g = open(index_path)
+
+        readme = read(f,String)
+        replace!(readme,"![](docs/src/img" => "![](img")
+        write(g,readme)
+    finally
+        close(f)
+        close(g)
+    end
+end
+
+readme2index()
+
 makedocs(;
     modules=[Taxonomy],
     authors="banhbio <ban@kuicr.kyoto-u.ac.jp>",
