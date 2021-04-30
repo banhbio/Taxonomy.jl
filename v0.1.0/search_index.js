fd17fa3b3dfbcdce39c2b8d990bb1606f3819900
Documenter.jl <documenter@juliadocs.github.io>
2021-04-30 15:56:39 +0000

    build based on fec5aac

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Taxonomy","category":"page"},{"location":"#Taxonomy","page":"Home","title":"Taxonomy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Taxonomy]","category":"page"}]
+}
