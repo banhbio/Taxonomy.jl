8eaf40f6f38e3c6024be7d0f9bd562382fba673a
banhbio <ban@kuicr.kyoto-u.ac.jp>
2022-09-25 17:26:08 +0900

    add Rank system

--- src/Taxonomy.jl
+++ src/Taxonomy.jl
@@ -1,24 +1,23 @@
 module Taxonomy
 using AbstractTrees
-using InteractiveUtils
 import DataAPI,
        DataAPI.All,
        DataAPI.Between,
        DataAPI.Cols
 export Rank, CanonicalRank, UnCanonicalRank,
-       species, genus, family, order, class, phylum, kingdom, superkingdom,
+       strain, subspecies, species, genus, family, order, class, phylum, kingdom, superkingdom,
        AbstractTaxon, Taxon, UnclassifiedTaxon,
        Lineage,
        print_tree,
-       taxid, name, rank, parent, get, children, lca,
+       taxid, name, rank, parent, get, children, lca, source,
        reformat, print_lineage, isdescendant, isancestor,
        All, Between, Cols,
        From, Until
 
 include("DataAPI.jl")
-include("rank.jl")
 include("database.jl")
 include("taxon.jl")
+include("rank.jl")
 include("lineage.jl")
 include("lca.jl")
 
--- src/rank.jl
+++ src/rank.jl
@@ -3,15 +3,25 @@ using InteractiveUtils
 abstract type Rank end
 abstract type CanonicalRank <: Rank end
 
-const CanonicalRanks = [:species, :genus, :family, :order, :class, :phylum, :kingdom, :superkingdom]
+const CanonicalRanks = [:strain, :subspecies, :species, :genus, :family, :order, :class, :phylum, :kingdom, :superkingdom]
 
-for (i, rank) in enumerate(CanonicalRanks)
+for (i, rank) in enumerate(CanonicalRanks[3:end])
     @eval begin
         struct $rank <: CanonicalRank end
         rank(::$rank) = Symbol($rank)
         Base.Integer(::$rank) = $i
     end
 end
+
+struct strain <: CanonicalRank end
+rank(::strain) = :strain
+Base.Integer(::strain) = 0
+
+struct subspecies <: CanonicalRank end
+rank(::subspecies) = :subspecies
+Base.Integer(::subspecies) = 0
+
+Base.Integer(T::Type{<:CanonicalRank}) = Integer(T())
 
 function Rank(s::Symbol)
     if s in CanonicalRanks
@@ -21,10 +31,6 @@ function Rank(s::Symbol)
     end
 end
 
-Base.Integer(T::Type{<:CanonicalRank}) = Integer(T())
-Base.isless(x1::CanonicalRank, x2::CanonicalRank) = isless(Integer(x1), Integer(x2))
-Base.isless(x1::Type{<:CanonicalRank}, x2::Type{<:CanonicalRank}) = isless(x1(), x2())
-
 const CanonicalRankSet = subtypes(CanonicalRank)
 
 struct UnCanonicalRank <: Rank
@@ -33,3 +39,19 @@ end
 
 rank(ucr::UnCanonicalRank) = ucr.rank
 Base.show(io::IO, r::Rank) = print(io, String(rank(r)))
+
+Base.isless(x1::CanonicalRank, x2::CanonicalRank) = isless(Integer(x1), Integer(x2))
+Base.isless(x1::Type{<:CanonicalRank}, x2::Type{<:CanonicalRank}) = isless(x1(), x2())
+
+function Base.isless(x1::AbstractTaxon, x2::CanonicalRank)
+    p = AbstractTrees.parent(x1)
+    while true
+        isnothing(x1) && return false
+        r = rank(x1) |> Rank
+        r isa CanonicalRank && return isless(r, x2)
+        x1 = p
+        p = AbstractTrees.parent(x1)
+    end
+end
+
+Base.isless(x1::AbstractTaxon, x2::Type{<:CanonicalRank}) = isless(x1, x2())
